47e819351f32b2a252f9b4dfde4e43e9cc649f17
Ricardo Horning Hammerschmidt <130876369+RicardoHorningHammerschmidt@users.noreply.github.com>
2023-10-27 20:00:58 -0300

    Add files via upload

--- entities/Researchs.java
+++ entities/Researchs.java
@@ -7,22 +7,19 @@ import java.util.List;
 public class Researchs {
 
     private Ativo ativo;
-    private LocalDate dataEntrada;
-    private LocalDate dataSaida;
-    private Character operacao;
-    private Double porcentagemCarteira;
     private Double valorInvestido;
-    private List<Double> entradas = new ArrayList<>();
-    private Double precoMedio;
-    private Double stop;
-    private Double parcial;
-    private Double alvo;
+
     private Integer tempoTranding;
     private Double retorno;
-    private Integer quantidadeAcoes;
     private Double resultadoFinanceiro;
 
-
+    public Researchs(Ativo ativo, Double valorInvestido, Integer tempoTranding, Double retorno, Double resultadoFinanceiro) {
+        this.ativo = ativo;
+        this.valorInvestido = valorInvestido;
+        this.tempoTranding = tempoTranding;
+        this.retorno = retorno;
+        this.resultadoFinanceiro = resultadoFinanceiro;
+    }
 
     public Ativo getAtivo() {
         return ativo;
@@ -32,68 +29,12 @@ public class Researchs {
         this.ativo = ativo;
     }
 
-    public LocalDate getDataEntrada() {
-        return dataEntrada;
+    public Double getValorInvestido() {
+        return valorInvestido;
     }
 
-    public void setDataEntrada(LocalDate dataEntrada) {
-        this.dataEntrada = dataEntrada;
-    }
-
-    public LocalDate getDataSaida() {
-        return dataSaida;
-    }
-
-    public void setDataSaida(LocalDate dataSaida) {
-        this.dataSaida = dataSaida;
-    }
-
-    public Character getOperacao() {
-        return operacao;
-    }
-
-    public void setOperacao(Character operacao) {
-        this.operacao = operacao;
-    }
-
-    public Double getPorcentagemCarteira() {
-        return porcentagemCarteira;
-    }
-
-    public void setPorcentagemCarteira(Double porcentagemCarteira) {
-        this.porcentagemCarteira = porcentagemCarteira;
-    }
-
-    public Double getPrecoMedio() {
-        return precoMedio;
-    }
-
-    public void setPrecoMedio(Double precoMedio) {
-        this.precoMedio = precoMedio;
-    }
-
-    public Double getStop() {
-        return stop;
-    }
-
-    public void setStop(Double stop) {
-        this.stop = stop;
-    }
-
-    public Double getParcial() {
-        return parcial;
-    }
-
-    public void setParcial(Double parcial) {
-        this.parcial = parcial;
-    }
-
-    public Double getAlvo() {
-        return alvo;
-    }
-
-    public void setAlvo(Double alvo) {
-        this.alvo = alvo;
+    public void setValorInvestido(Double valorInvestido) {
+        this.valorInvestido = valorInvestido;
     }
 
     public Integer getTempoTranding() {
@@ -112,14 +53,6 @@ public class Researchs {
         this.retorno = retorno;
     }
 
-    public Integer getQuantidadeAcoes() {
-        return quantidadeAcoes;
-    }
-
-    public void setQuantidadeAcoes(Integer quantidadeAcoes) {
-        this.quantidadeAcoes = quantidadeAcoes;
-    }
-
     public Double getResultadoFinanceiro() {
         return resultadoFinanceiro;
     }
@@ -127,22 +60,4 @@ public class Researchs {
     public void setResultadoFinanceiro(Double resultadoFinanceiro) {
         this.resultadoFinanceiro = resultadoFinanceiro;
     }
-
-    public Double getValorInvestido() {
-        return valorInvestido;
-    }
-
-    public void setValorInvestido(Double valorInvestido) {
-        this.valorInvestido = valorInvestido;
-    }
-
-    public void addEntradas(double entrada) {
-        entradas.add(entrada);
-    }
-
-    public void removeEntradas(double entrada) {
-        entradas.remove(entrada);
-    }
-
-
 }
