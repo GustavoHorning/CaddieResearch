f109b112dbf4562afbc876023bb3b0b027ceec4a
Ricardo Horning Hammerschmidt <130876369+RicardoHorningHammerschmidt@users.noreply.github.com>
2023-10-27 20:01:37 -0300

    Add files via upload

--- entities/Main.java
+++ entities/Main.java
@@ -2,7 +2,6 @@ package entities;
 
 public class Main {
     public static void main(String[] args) {
-        // Chama a interface gráfica para cadastrar usuários
         CadastroUsuarioGUI cadastroUsuarioGUI = new CadastroUsuarioGUI();
         cadastroUsuarioGUI.setVisible(true);
     }
