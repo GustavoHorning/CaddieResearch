34da48c45e32712f3abf4484387386d36d87444f
Ricardo Horning Hammerschmidt <130876369+RicardoHorningHammerschmidt@users.noreply.github.com>
2023-11-13 01:47:31 -0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
 package entities;
 
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -26,7 +27,7 @@ public class Main extends JFrame implements ComparacaoResearchs {
     private double retornoPorcentagemResearch1 = 0;
     private double retornoPorcentagemResearch2 = 0;
     private double retornoPorcentagemResearch3 = 0;
-
+    private double confirmacaoSair = 0;
 
     public Main() {
         setTitle("Cadastro de Usuário e Seleção de Arquivo");
@@ -115,6 +116,10 @@ public class Main extends JFrame implements ComparacaoResearchs {
         if (usuariosCadastrados == 3) {
             cadastrarButton.setEnabled(false);
             exibirMensagemLimiteAtingido();
+            confirmacaoSair += 1;
+            if (confirmacaoSair == 2){
+                sairButton.setEnabled(true);
+            }
         }
 
         System.out.println("Usuário cadastrado: " + nome + " (CPF: " + cpf + ", Data de Nascimento: " + dataNascimentoStr + ", Valor Investido: " + valorInvestido + ")");
@@ -127,105 +132,115 @@ public class Main extends JFrame implements ComparacaoResearchs {
 
     private void exibirMensagemLimiteAtingido() {
         JOptionPane.showMessageDialog(this, "Limite de três usuários atingido.", "Limite Atingido", JOptionPane.INFORMATION_MESSAGE);
-        sairButton.setEnabled(true);
     }
 
     private void selecionarArquivo() {
-        List<Double> retornoResearch1 = new ArrayList<>();
-        List<Double> retornoResearch2 = new ArrayList<>();
-        List<Double> retornoResearch3 = new ArrayList<>();
-        BancoCentralFicticio bancoCentralFicticio = new BancoCentralFicticio("Banco central ficticio");
-        if (fileCount < 3) {
-            int resultado = fileChooser.showOpenDialog(null);
+        JFileChooser fileChooser = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("Arquivos CSV", "csv");
+        fileChooser.setFileFilter(filter);
+        fileChooser.setAcceptAllFileFilterUsed(false);
 
-            if (resultado == JFileChooser.APPROVE_OPTION) {
-                List<Researchs> researchsList = new ArrayList<>();
+        int resultado = fileChooser.showOpenDialog(null);
+        if (resultado == JFileChooser.APPROVE_OPTION) {
 
-                try {
-                    BufferedReader br = new BufferedReader(new FileReader(fileChooser.getSelectedFile()));
-                    String line;
-
-                    while ((line = br.readLine()) != null) {
-                        String[] data = line.split(";");
-
-                        if (data.length >= 6) {
-                            String ticket = data[0];
-                            String nomeEmpresa = data[1];
-                            double valorInvestido = extrairValor(data[2]);
-                            int tempoTrading = Integer.parseInt(data[3]);
-                            double retorno = extrairPorcentagem(data[4]);
-                            double resultadoFinanceiro = extrairValor(data[5]);
-
-                            Researchs research = new Researchs(new Ativo(ticket, nomeEmpresa), valorInvestido, tempoTrading, retorno, resultadoFinanceiro);
-                            researchsList.add(research);
-                        }
-                    }
-
-                    br.close();
-
-                    if (!researchsList.isEmpty()) {
-                        for (Researchs research : researchsList) {
-                            System.out.println("Ticket: " + research.getAtivo().getTicket());
-                            System.out.println("Nome da Empresa: " + research.getAtivo().getNomeEmpresa());
-                            System.out.println("Valor Investido: " + research.getValorInvestido());
-                            System.out.println("Tempo de Trading: " + research.getTempoTranding());
-                            System.out.println("Retorno: " + research.getRetorno());
-                            System.out.println("Resultado Financeiro: " + research.getResultadoFinanceiro());
-                            System.out.println();
-                            System.out.printf("Calculo de taxa de corretagem do(a) empresa: %s - (ticket: %s): ",
-                                    research.getAtivo().getNomeEmpresa(),research.getAtivo().getTicket());
-                            System.out.printf("%.2f\n",bancoCentralFicticio.calculoTaxaCorretagem(research.getValorInvestido()));
-                            System.out.printf("Calculo de imposto do(a) empresa: %s - (ticket: %s): ",
-                                    research.getAtivo().getNomeEmpresa(),research.getAtivo().getTicket());
-                            System.out.printf("%.2f %%\n",bancoCentralFicticio.calculoImposto(research.getTempoTranding(),research.getResultadoFinanceiro()));
+            if (fileCount < 3) {
+                List<Double> retornoResearch1 = new ArrayList<>();
+                List<Double> retornoResearch2 = new ArrayList<>();
+                List<Double> retornoResearch3 = new ArrayList<>();
+                BancoCentralFicticio bancoCentralFicticio = new BancoCentralFicticio("Banco central ficticio");
 
 
-                            System.out.println();
+                    List<Researchs> researchsList = new ArrayList<>();
 
-                            if (fileCount == 0) {
-                                retornoResearch1.add(research.getRetorno());
-                                retornoPorcentagemResearch1 += research.getRetorno();
-                            } else if (fileCount == 1) {
-                                retornoResearch2.add(research.getRetorno());
-                                retornoPorcentagemResearch2 += research.getRetorno();
-                            } else if (fileCount == 2) {
-                                retornoResearch3.add(research.getRetorno());
-                                retornoPorcentagemResearch3 += research.getRetorno();
+                    try {
+                        BufferedReader br = new BufferedReader(new FileReader(fileChooser.getSelectedFile()));
+                        String line;
+
+                        while ((line = br.readLine()) != null) {
+                            String[] data = line.split(";");
+
+                            if (data.length >= 6) {
+                                String ticket = data[0];
+                                String nomeEmpresa = data[1];
+                                double valorInvestido = extrairValor(data[2]);
+                                int tempoTrading = Integer.parseInt(data[3]);
+                                double retorno = extrairPorcentagem(data[4]);
+                                double resultadoFinanceiro = extrairValor(data[5]);
+
+                                Researchs research = new Researchs(new Ativo(ticket, nomeEmpresa), valorInvestido, tempoTrading, retorno, resultadoFinanceiro);
+                                researchsList.add(research);
                             }
                         }
-                        fileCount++;
-                    }
 
-                    if (fileCount == 3) {
-                        sairButton.setEnabled(true);
-                        JOptionPane.showMessageDialog(null, "Limite de arquivos atingido.");
-                    }
+                        br.close();
 
-                    if (fileCount >= 3) {
-                        selecionarArquivoButton.setEnabled(false);
-                        System.out.printf("%.2f\n",retornoPorcentagemResearch1);
-                        System.out.printf("%.2f\n",retornoPorcentagemResearch2);
-                        System.out.printf("%.2f\n",retornoPorcentagemResearch3);
+                        if (!researchsList.isEmpty()) {
+                            for (Researchs research : researchsList) {
+                                System.out.println("Ticket: " + research.getAtivo().getTicket());
+                                System.out.println("Nome da Empresa: " + research.getAtivo().getNomeEmpresa());
+                                System.out.println("Valor Investido: " + research.getValorInvestido());
+                                System.out.println("Tempo de Trading: " + research.getTempoTranding());
+                                System.out.println("Retorno: " + research.getRetorno());
+                                System.out.println("Resultado Financeiro: " + research.getResultadoFinanceiro());
+                                System.out.println();
+                                System.out.printf("Calculo de taxa de corretagem do(a) empresa: %s - (ticket: %s): ",
+                                        research.getAtivo().getNomeEmpresa(), research.getAtivo().getTicket());
+                                System.out.printf("%.2f\n", bancoCentralFicticio.calculoTaxaCorretagem(research.getValorInvestido()));
+                                System.out.printf("Calculo de imposto do(a) empresa: %s - (ticket: %s): ",
+                                        research.getAtivo().getNomeEmpresa(), research.getAtivo().getTicket());
+                                System.out.printf("%.2f %%\n", bancoCentralFicticio.calculoImposto(research.getTempoTranding(), research.getResultadoFinanceiro()));
 
 
-                        int melhorResearch = maiorValor(retornoPorcentagemResearch1,retornoPorcentagemResearch2,retornoPorcentagemResearch3);
+                                System.out.println();
 
-                        if (melhorResearch == 0) {
-                            System.out.println("Research 1 ganhou");
-                        } else if (melhorResearch == 1) {
-                            System.out.println("Research 2 ganhou");
-                        } else {
-                            System.out.println("Research 3 ganhou");
+                                if (fileCount == 0) {
+                                    retornoResearch1.add(research.getRetorno());
+                                    retornoPorcentagemResearch1 += research.getRetorno();
+                                } else if (fileCount == 1) {
+                                    retornoResearch2.add(research.getRetorno());
+                                    retornoPorcentagemResearch2 += research.getRetorno();
+                                } else if (fileCount == 2) {
+                                    retornoResearch3.add(research.getRetorno());
+                                    retornoPorcentagemResearch3 += research.getRetorno();
+                                }
+                            }
+                            fileCount++;
                         }
 
+                        if (fileCount == 3) {
+                            JOptionPane.showMessageDialog(null, "Limite de arquivos atingido.");
+                            confirmacaoSair += 1;
+                            if (confirmacaoSair == 2){
+                                sairButton.setEnabled(true);
+                            }
+                        }
+
+                        if (fileCount >= 3) {
+                            selecionarArquivoButton.setEnabled(false);
+                            System.out.printf("%.2f\n", retornoPorcentagemResearch1);
+                            System.out.printf("%.2f\n", retornoPorcentagemResearch2);
+                            System.out.printf("%.2f\n", retornoPorcentagemResearch3);
+
+
+                            int melhorResearch = maiorValor(retornoPorcentagemResearch1, retornoPorcentagemResearch2, retornoPorcentagemResearch3);
+
+                            if (melhorResearch == 0) {
+                                System.out.println("Research 1 ganhou");
+                            } else if (melhorResearch == 1) {
+                                System.out.println("Research 2 ganhou");
+                            } else {
+                                System.out.println("Research 3 ganhou");
+                            }
+
+                        }
+
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
                     }
 
-                } catch (IOException ex) {
-                    ex.printStackTrace();
-                }
+            } else {
+                JOptionPane.showMessageDialog(null, "Limite de arquivos atingido.");
             }
-        } else {
-            JOptionPane.showMessageDialog(null, "Limite de arquivos atingido.");
         }
     }
 
